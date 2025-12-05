2d51dd6305e57032eaed831deb4f51ee25762782
vinayakbhat123 <abcdbhat014@gmail.com>
2025-12-05 22:41:03 +0530

    add USER_LOGO

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,4 +1,4 @@
-import { AppWindowIcon, CodeIcon } from "lucide-react";
+import { AppWindowIcon, CodeIcon, Loader2, User, User2Icon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import {
@@ -13,10 +13,78 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { BASE_URL, USERLOGO } from "@/utils/constant";
+import { toast } from "sonner";
+import axios from "axios";
+import { addUser } from "@/Redux/userSlice";
 
 const Profile = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
   const params = useParams();
-  const userId = params;
+  const userId = params.userId;
+  const [showloading,setShowLoading] = useState(false)
+  const [updateUser, setUpdateUser] = useState({
+    firstName: user?.firstName || "",
+    lastName: user?.lastName || "",
+    emailId: user?.emailId || "",
+    address: user?.address || "",
+    phoneNo: user?.phoneNo || "",
+    zipCode: user?.zipCode || "", 
+    photoUrl: user?.photoUrl || "",
+    role: user?.role || "",
+  });
+
+  const [file, setFile] = useState(null);
+
+  const handleChange = (e) => {
+    setUpdateUser({ ...updateUser, [e.target.name]: e.target.value });
+  };
+
+  const handleFileChange = (e) => {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+    setUpdateUser({
+      ...updateUser,
+      photoUrl: URL.createObjectURL(selectedFile),
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      //use formData() for text + file
+      setShowLoading(true)
+      const formData = new FormData();
+      formData.append("firstName", updateUser.firstName);
+      formData.append("lastName", updateUser.lastName);
+      formData.append("emailId", updateUser.emailId);
+      formData.append("phoneNo", updateUser.phoneNo);
+      formData.append("address", updateUser.address);
+      formData.append("city", updateUser.city);
+      formData.append("zipCode", updateUser.zipCode);
+      formData.append("role", updateUser.role);
+
+      if(file) {
+         formData.append("file",file)
+      }
+
+      const res = await axios.patch(BASE_URL +"/update/"+userId,formData,{withCredentials:true})
+      console.log(res?.data?.user)
+      if(res?.data?.success){
+        toast.success(res?.data?.message)
+        dispatch(addUser(res?.data?.user))
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Falied to update profile");
+    }finally{
+      setShowLoading(false)
+    }
+  };
+
   return (
     <div className="pt-20 min-h-screen bg-gray-100">
       <Tabs
@@ -37,7 +105,7 @@ const Profile = () => {
                 {/* profile picture */}
                 <div className="flex flex-col items-center">
                   <img
-                    src="/vite.svg"
+                    src={updateUser.photoUrl || USERLOGO}
                     alt="profile"
                     className="w-32 h-32 rounded-full object-cover
                   border-4 border-pink-800"
@@ -48,11 +116,19 @@ const Profile = () => {
                     }
                   >
                     Change Picture
-                    <input type="file" accept="image/*" className="hidden" />
+                    <input
+                      type="file"
+                      accept="image/*"
+                      className="hidden"
+                      onChange={handleFileChange}
+                    />
                   </Label>
                 </div>
                 {/* profile form */}
-                <form className="space-y-2 shadow-lg p-5 bg-white ">
+                <form
+                  onSubmit={handleSubmit}
+                  className="space-y-2 shadow-lg p-5 bg-white "
+                >
                   <div className="grid grid-cols-2 gap-2">
                     <div className="">
                       <Label className={"block text-sm font-medium"}>
@@ -61,7 +137,9 @@ const Profile = () => {
                       <Input
                         type={"text"}
                         name={"firstName"}
-                         placeholder={"Enter your Firstname"}
+                        placeholder={"Enter your Firstname"}
+                        value={updateUser?.firstName}
+                        onChange={handleChange}
                         className={"w-full border rounded-lg"}
                       />
                     </div>
@@ -72,7 +150,9 @@ const Profile = () => {
                       <Input
                         type={"text"}
                         name={"lastName"}
-                         placeholder={"Enter your Lastname"}
+                        value={updateUser?.lastName}
+                        onChange={handleChange}
+                        placeholder={"Enter your Lastname"}
                         className={"w-full border rounded-lg"}
                       />
                     </div>
@@ -82,6 +162,8 @@ const Profile = () => {
                     <Input
                       type={"email"}
                       name={"email"}
+                      value={updateUser?.emailId}
+                      onChange={handleChange}
                       disabled
                       className={
                         "w-full border rounded-lg px-3 py-2 mt-1 bg-gray-100 cursor-not-allowed"
@@ -95,50 +177,66 @@ const Profile = () => {
                     <Input
                       type={"text"}
                       name={"phoneno"}
+                      value={updateUser?.phoneNo}
+                      onChange={handleChange}
                       placeholder={"Enter your Contact no"}
                       className={"w-full border rounded-lg px-3 py-2 mt-1"}
                     />
                   </div>
-                    <div className="">
+                  <div className="">
                     <Label className={"block text-sm font-medium"}>
                       Address
                     </Label>
                     <Input
                       type={"text"}
                       name={"address"}
+                      value={updateUser?.address}
+                      onChange={handleChange}
                       placeholder={"Enter your Address"}
                       className={"w-full border rounded-lg px-3 py-2 mt-1"}
                     />
                   </div>
-                    <div className="">
-                    <Label className={"block text-sm font-medium"}>
-                      City
-                    </Label>
+                  <div className="grid grid-cols-2 gap-2">
+                     <div className="">
+                    <Label className={"block text-sm font-medium"}>City</Label>
                     <Input
                       type={"text"}
                       name={"city"}
+                      value={updateUser?.city}
+                      onChange={handleChange}
                       placeholder={"Enter your City"}
                       className={"w-full border rounded-lg px-3 py-2 mt-1"}
                     />
                   </div>
-                    <div className="">
+                  <div className="">
                     <Label className={"block text-sm font-medium"}>
                       zip code
                     </Label>
                     <Input
                       type={"text"}
                       name={"zipcode"}
+                      value={updateUser?.zipCode}
+                      onChange={handleChange}
                       placeholder={"Enter your zip code"}
                       className={"w-full border rounded-lg px-3 py-2 mt-1"}
                     />
                   </div>
-                  <Button className={"bg-red-500 hover:bg-red-700 text-white rounded-lg w-full mt-4 py-2 font-semibold"}>Update Profile</Button>
+                  </div>
+                 
+                  <Button
+                    type="submit"
+                    className={
+                      "bg-red-500 hover:bg-red-700 text-white rounded-lg w-full mt-4 py-2 font-semibold"
+                    }
+                  >
+                   {showloading ?<> <Loader2 className="h-4 w-4 animate-spin mr-2"/></> : "Update Profile"}
+                  </Button>
                 </form>
               </div>
             </div>
           </div>
         </TabsContent>
-        <TabsContent value="orders"> 
+        <TabsContent value="orders">
           <Card>
             <CardHeader>
               <CardTitle>Orders</CardTitle>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,2 @@
 export const BASE_URL = "http://localhost:8000"
+export const USERLOGO = "https://geographyandyou.com/images/user-profile.png"
